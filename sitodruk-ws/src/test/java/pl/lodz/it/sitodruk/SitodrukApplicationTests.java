ab71953d360e3c505cfd61a3efb7fd87e1ee8b30
Krystian Kowalski <krystian.kowalski95@gmail.com>
2020-11-07 19:55:37 +0100

    Usunięcie starych importów

--- sitodruk-ws/src/test/java/pl/lodz/it/sitodruk/SitodrukApplicationTests.java
+++ sitodruk-ws/src/test/java/pl/lodz/it/sitodruk/SitodrukApplicationTests.java
@@ -4,17 +4,9 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import pl.lodz.it.sitodruk.config.JwtUtils;
-import pl.lodz.it.sitodruk.model.UserEntity;
-import pl.lodz.it.sitodruk.repositories.UserRepository;
-import pl.lodz.it.sitodruk.service.UserService;
+
 
 @RunWith(MockitoJUnitRunner.class)
 class SitodrukApplicationTests {
