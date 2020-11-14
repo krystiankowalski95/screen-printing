45793687f163313436c2ee5fcf64ae312a3bb2ac
Krystian Kowalski <216808@edu.p.lodz.pl>
2020-11-14 20:18:42 +0100

    Dodanie testu łączacego się z API PayU uzyskującego Token

--- sitodruk-ws/src/test/java/pl/lodz/it/sitodruk/SitodrukApplicationTests.java
+++ sitodruk-ws/src/test/java/pl/lodz/it/sitodruk/SitodrukApplicationTests.java
@@ -1,26 +1,43 @@
 package pl.lodz.it.sitodruk;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
-import pl.lodz.it.sitodruk.config.JwtUtils;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.*;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
 
-@RunWith(MockitoJUnitRunner.class)
-class SitodrukApplicationTests {
+import java.util.Map;
 
-	JwtUtils jwtUtils;
 
-	@Before
-	void init(){
-	    jwtUtils = new JwtUtils();
-	}
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SitodrukApplicationTests {
 
-	@Test
-	void testMethod() {
-		Assert.assertEquals(false, false);
-	}
+    @Value("${thesis.app.payu.client.id}")
+    private String client_id;
+    @Value("${thesis.app.payu.client.secret}")
+    private String client_secret;
+    @Value("${thesis.app.payu.url}")
+    private String url;
+
+
+
+
+    @Test
+    public void hasAPIGeneratedToken()   {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        String postingString = "grant_type=client_credentials&client_id="+client_id+"&client_secret="+client_secret;
+        HttpEntity<?> entity = new HttpEntity<>(postingString, headers);
+        ResponseEntity<Map> responseEntity = restTemplate.exchange(url,HttpMethod.POST,entity,Map.class);
+        Assertions.assertEquals(HttpStatus.OK.toString(),responseEntity.getStatusCode().toString());
+    }
 
 }
