f04b853d7dc5fde00f89f11def60daeec6ab3980
Krystian Kowalski <216808@edu.p.lodz.pl>
2021-01-01 21:46:40 +0100

    Dodanie modelu adresu

---
+++ sitodruk-ws/src/main/sitodruk-web/src/models/address.js
@@ -0,0 +1,10 @@
+export default class Address {
+    constructor(country, voivodeship, city, postalCode, street, streetNumber) {
+      this.country = country;
+      this.voivodeship = voivodeship;
+      this.city = city;
+      this.postalCode = postalCode;
+      this.street = street;
+      this.streetNumber = streetNumber;
+    }
+  }
