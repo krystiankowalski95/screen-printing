526a907c6277d05cd5716345c1873bed24b8ec22
Krystian Kowalski <216808@edu.p.lodz.pl>
2021-01-24 11:30:29 +0100

    Dodanie zdjęć na stronę główną

--- sitodruk-ws/src/main/sitodruk-web/src/views/Home.vue
+++ sitodruk-ws/src/main/sitodruk-web/src/views/Home.vue
@@ -1,9 +1,21 @@
 <template>
   <div class="container">
-    <header class="jumbotron">
-      <h3>{{content}}</h3>
-      <h3>{{ $t('hello') }}</h3>
-    </header>
+    <b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="4000"
+      controls
+      indicators
+      background="#ababab"
+      img-width="1024"
+      img-height="480"
+      style="text-shadow: 1px 1px 2px #333;"
+    >
+      <b-carousel-slide img-src="https://cdn.pixabay.com/photo/2016/03/09/11/33/silk-screen-1246169_1280.jpg" />
+      <b-carousel-slide img-src="https://cdn.pixabay.com/photo/2016/04/21/01/21/paintings-1342452_1280.jpg"/>
+      <b-carousel-slide img-src="https://cdn.pixabay.com/photo/2017/09/30/02/25/painting-2801050_1280.jpg"/>
+      <b-carousel-slide img-src="https://cdn.pixabay.com/photo/2017/07/03/20/17/abstract-2468874_1280.jpg"/>
+    </b-carousel>
   </div>
 </template>
 
@@ -17,6 +29,6 @@ export default {
     };
   },
   mounted() {
-  }
+  },
 };
 </script>
